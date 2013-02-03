80b1fe7130d528e5f28187d2a7649356257b88d0
Carel <careld@voxtelecom.co.za>
2013-02-03 16:13:51 +0200

    added to do list model Signed-off-by: Carel <careld@voxtelecom.co.za>

--- LeverageApi/Controllers/version1/HomeController.cs
+++ LeverageApi/Controllers/version1/HomeController.cs
@@ -5,35 +5,28 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace LeverageApi.Controllers.Version1
-{
-    public class HomeController : ApiController
-    {
-        // GET api/home
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+namespace LeverageApi.Controllers.Version1 {
+	public class HomeController : ApiController {
+		// GET api/home
+		public IEnumerable<string> Get() {
+			return new string[] { "value1", "value2" };
+		}
 
-        // GET api/home/5
-        public string Get(int id)
-        {
-            return "value";
-        }
+		// GET api/home/5
+		public string Get(int id) {
+			return "value";
+		}
 
-        // POST api/home
-        public void Post([FromBody]string value)
-        {
-        }
+		// POST api/home
+		public void Post([FromBody]string value) {
+		}
 
-        // PUT api/home/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+		// PUT api/home/5
+		public void Put(int id, [FromBody]string value) {
+		}
 
-        // DELETE api/home/5
-        public void Delete(int id)
-        {
-        }
-    }
+		// DELETE api/home/5
+		public void Delete(int id) {
+		}
+	}
 }
---
+++ LeverageApi/Models/ToDoList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeverageApi.Models {
+	public class ToDoList {
+
+			//["Hidden"]
+			public int Id { get; set; }
+
+			//["Require"]
+			public string Title { get; set; }
+
+			public DateTime DueDate { get; set; }
+
+			public bool Completed { get; set; }
+	
+	}
+}

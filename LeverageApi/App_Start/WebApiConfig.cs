d5600c57008c8e80cfe3f6f23aba1093cef79e49
Carel <careld@voxtelecom.co.za>
2013-02-03 12:40:19 +0200

    adding leverage api with versioning Signed-off-by: Carel <careld@voxtelecom.co.za>

---
+++ LeverageApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace LeverageApi {
+	public static class WebApiConfig {
+		public static void Register(HttpConfiguration config) {
+		
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{version}/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+		}
+	}
+}
---
+++ LeverageApi/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using System.Web.Mvc;
+using System.Web.Routing;
+using SDammann.WebApi.Versioning;
+
+namespace LeverageApi {
+	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+	// visit http://go.microsoft.com/?LinkId=9394801
+	public class MvcApplication : System.Web.HttpApplication {
+		protected void Application_Start() {
+
+			GlobalConfiguration.Configuration.Services.Replace(typeof(IHttpControllerSelector),
+																													new RouteVersionedControllerSelector(GlobalConfiguration.Configuration));
+
+			AreaRegistration.RegisterAllAreas();
+
+			WebApiConfig.Register(GlobalConfiguration.Configuration);
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+		}
+	}
+}
+

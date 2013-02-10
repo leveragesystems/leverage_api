bf83669c829342189c187ec8d388a7c901d61e8b
michaelmarriott-vox <mmarriott_sa@hotmail.com>
2013-02-10 15:27:17 +0200

    bin stuff

--- LeverageApi/Global.asax.cs
+++ LeverageApi/Global.asax.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http.Dispatcher;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -14,8 +16,11 @@ namespace LeverageApi {
 	public class MvcApplication : System.Web.HttpApplication {
 		protected void Application_Start() {
 
-			GlobalConfiguration.Configuration.Services.Replace(typeof(IHttpControllerSelector),
-																													new RouteVersionedControllerSelector(GlobalConfiguration.Configuration));
+      GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+      GlobalConfiguration.Configuration.Formatters[0].SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
+
+			//GlobalConfiguration.Configuration.Services.Replace(typeof(IHttpControllerSelector),
+				//																									new RouteVersionedControllerSelector(GlobalConfiguration.Configuration));
 
 			AreaRegistration.RegisterAllAreas();
 

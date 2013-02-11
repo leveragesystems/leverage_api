d4a3f069cd38149e5cd02ee8ca80ea784f6683d8
Carel <dutoit.carel7@gmail.com>
2013-02-11 23:42:28 +0200

    Mongo db added using repository patern generic
    Signed-off-by: Carel <dutoit.carel7@gmail.com>

---
+++ LeverageApi/Controllers/version1/PersonController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DbLayer.Models;
+using DbLayer.Repositories;
+
+namespace LeverageApi.Controllers.version1
+{
+    public class PersonController : ApiController
+    {
+      MongoRepository<Person,int> db = new MongoRepository<Person, int>() {
+        ConnectionString = "mongodb://localhost/database"
+      };
+
+      public HttpResponseMessage PostPerson(Person model) {
+        if (ModelState.IsValid) {
+          db.Create(model);
+
+          HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, model);
+          response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = model.Id }));
+          return response;
+        } else {
+          return Request.CreateResponse(HttpStatusCode.BadRequest);
+        }
+      }
+
+      public HttpResponseMessage DeletePerson(int person_id) {
+        
+        var dbModel = db.SingleOrDefault(p => p.Id == person_id);
+        if (dbModel == null) {
+          return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+        db.Delete(person_id);
+
+        return Request.CreateResponse(HttpStatusCode.OK, dbModel);
+      }
+    }
+}

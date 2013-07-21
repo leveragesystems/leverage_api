0d3e437d33e739a0f7c0f1e6f5a6032688f30dfc
michaelmarriott-vox <mmarriottsa@gmail.com>
2013-07-21 21:52:48 +0200

    Seed Console

---
+++ LeverageConsole/Seed.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbLayer.Models;
+using DbLayer.Repositories;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace LeverageApi {
+
+  public class Seed {
+
+    DocumentStore documentStore;
+
+    public Seed() {
+
+        documentStore = new DocumentStore {
+            Url = "http://localhost:8080",
+            DefaultDatabase = "WebApi"
+        };
+        documentStore.Initialize();
+    }
+
+    public void Create() {
+
+        // Saving changes using the session API
+        using (IDocumentSession session = documentStore.OpenSession()) {
+            // Operations against session
+            UserLogin userLogin = new UserLogin();
+            userLogin.Username = "dev";
+            userLogin.Password = "dev";
+            session.Store(userLogin);
+            // Flush those changes
+            session.SaveChanges();
+        }
+      Console.WriteLine("Completed");
+
+    }
+  }
+}
